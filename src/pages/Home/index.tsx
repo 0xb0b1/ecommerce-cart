c6585e0e53f01d78ffc4511145bf0f668963f8d3
Paulo Vicente <vcente82@gmail.com>
2022-04-28 19:16:02 -0300

    show products from api

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,69 @@
-import { ReactNode } from "react";
+import { useEffect, useState } from "react";
+import { MdAddShoppingCart } from "react-icons/md";
+import { api } from "../../services/api";
+import { formatPrice } from "../../util/formatCurrency";
 
-import { Container } from "./styles";
+import { ProductList } from "./styles";
 
-interface HomeProps {
-  children: ReactNode;
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  image: string;
 }
 
-function Home() {
+interface ProductFormatted extends Product {
+  priceFormatted: string;
+}
+
+interface CartItemsAmount {
+  [key: number]: number;
+}
+
+const Home = (): JSX.Element => {
+  const [products, setProducts] = useState<ProductFormatted[]>([]);
+  // const { addProduct, cart } = useCart();
+
+  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
+  //   // TODO
+  // }, {} as CartItemsAmount)
+
+  useEffect(() => {
+    async function loadProducts() {
+      const response = await api.get("/products");
+      setProducts(response.data);
+    }
+
+    loadProducts();
+  }, []);
+
+  function handleAddProduct(id: number) {
+    // TODO
+  }
+
   return (
-    <Container>
-      <h1>Home</h1>
-    </Container>
+    <ProductList>
+      {products?.map((product) => (
+        <li key={product.id}>
+          <img src={product.image} alt={product.title} />
+          <strong>{product.title}</strong>
+          <span>{formatPrice(product.price)}</span>
+          <button
+            type="button"
+            data-testid="add-product-button"
+            // onClick={() => handleAddProduct(product.id)}
+          >
+            <div data-testid="cart-product-quantity">
+              <MdAddShoppingCart size={16} color="#FFF" />
+              {/* {cartItemsAmount[product.id] || 0} */}2
+            </div>
+
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+      ))}
+    </ProductList>
   );
-}
+};
 
 export default Home;
